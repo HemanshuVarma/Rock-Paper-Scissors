6e3f0e96adf25a26e968881d62f865ad5d6efc3a
Hemanshu Varma <hemanshuvarma916@gmail.com>
2021-08-19 12:48:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/varma/hemanshu/rockpaperscissor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.varma.hemanshu.rockpaperscissor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
